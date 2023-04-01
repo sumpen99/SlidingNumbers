c9024e404cce73ab57aae120a9670ea72ac74946
sumpen99 <sumpen841001@hotmail.com>
2023-04-01 12:07:18 +0200

    Initial Commit

---
+++ SlidingNumbers/SlidingNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingNumbersApp.swift
+//  SlidingNumbers
+//
+//  Created by fredrik sundström on 2023-04-01.
+//
+
+import SwiftUI
+
+@main
+struct SlidingNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
